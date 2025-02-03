fd5f8be1702ed61fcc7b2ce4ffd2c753977ec2d7
Gunawan Aschari <gunawan.aschari@gmail.com>
2025-02-03 13:49:52 +0700

    First commit

---
+++ app/src/main/java/com/gunawan/webaddressnotes/activity/MainActivity.java
@@ -0,0 +1,114 @@
+package com.gunawan.webaddressnotes.activity;
+
+import android.content.DialogInterface;
+import android.database.Cursor;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.ViewCompat;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.google.android.material.bottomsheet.BottomSheetDialog;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.gunawan.webaddressnotes.R;
+import com.gunawan.webaddressnotes.adapter.WebAddressAdapter;
+import com.gunawan.webaddressnotes.database.Database;
+import com.gunawan.webaddressnotes.model.WebAddress;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+    private RecyclerView recyclerWebAddress;
+    private FloatingActionButton fabAdd;
+    private Database db;
+    private WebAddressAdapter adapter;
+    private ArrayList<WebAddress> listWeb = new ArrayList<WebAddress>();
+    private Cursor cur;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        setupView();
+    }
+
+    private void setupView() {
+        recyclerWebAddress = (RecyclerView) findViewById(R.id.recyclerWebAddress);
+        fabAdd = (FloatingActionButton) findViewById(R.id.fab);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        db = new Database(this);
+        getDataWebAddress();
+        fabAdd.setOnClickListener(view -> showDialogFormWebAddress(false, 0));
+    }
+
+    private void getDataWebAddress() {
+        ViewCompat.setNestedScrollingEnabled(recyclerWebAddress, false);
+        recyclerWebAddress.setHasFixedSize(true);
+        recyclerWebAddress.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false));
+        listWeb.clear();
+        listWeb = db.getListWebAddress();
+        if(listWeb != null) {
+            adapter = new WebAddressAdapter(this, listWeb);
+            adapter.notifyDataSetChanged();
+            recyclerWebAddress.setAdapter(adapter);
+            adapter.setOnItemClickListener(new WebAddressAdapter.OnCustomItemClickListener() {
+                @Override
+                public void onEditClick(int position) {
+                    showDialogFormWebAddress(true, position);
+                }
+                @Override
+                public void onDeleteClick(int position) {
+                    db.deleteWebAddres(listWeb.get(position).getId());
+                    getDataWebAddress();
+                }
+            });
+        }
+    }
+
+    private void showDialogFormWebAddress(final boolean isEdit, final int position) {
+        final BottomSheetDialog mBottomSheetDialog = new BottomSheetDialog(this);
+        View sheetView = getLayoutInflater().inflate(R.layout.dialog_form_web_address, null);
+        mBottomSheetDialog.setContentView(sheetView);
+        mBottomSheetDialog.show();
+        final EditText etName = sheetView.findViewById(R.id.etName);
+        final EditText etAddress = sheetView.findViewById(R.id.etAddress);
+        Button btnSave = sheetView.findViewById(R.id.btnSave);
+
+        if(isEdit) {
+            etName.setText(listWeb.get(position).getName());
+            etAddress.setText(listWeb.get(position).getAddress());
+        }
+
+        btnSave.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(etName.getText().toString().isEmpty() || etAddress.getText().toString().isEmpty()) {
+                    AlertDialog.Builder ab = new AlertDialog.Builder(MainActivity.this);
+                    ab.setMessage("Please complete all fields.");
+                    ab.setNeutralButton("OK", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dlg, int sumthin) {}
+                    }).show();
+                }
+                else {
+                    if(isEdit) {
+                        db.updateWebAddres(listWeb.get(position).getId(), etName.getText().toString().trim(), etAddress.getText().toString().trim());
+                    }
+                    else {
+                        db.addWebAddress(etName.getText().toString().trim(), etAddress.getText().toString().trim());
+                    }
+                    mBottomSheetDialog.dismiss();
+                    getDataWebAddress();
+                }
+            }
+        });
+    }
+
+}
---
+++ app/src/main/java/com/gunawan/webaddressnotes/adapter/WebAddressAdapter.java
@@ -0,0 +1,117 @@
+package com.gunawan.webaddressnotes.adapter;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.appcompat.widget.PopupMenu;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.gunawan.webaddressnotes.R;
+import com.gunawan.webaddressnotes.model.WebAddress;
+
+import java.util.ArrayList;
+
+public class WebAddressAdapter extends RecyclerView.Adapter<WebAddressAdapter.ViewHolder> {
+    private ArrayList<WebAddress> wa;
+    private Activity activity;
+    private OnCustomItemClickListener mListener;
+
+    public WebAddressAdapter(Activity activity, ArrayList<WebAddress> wa) {
+        this.wa = wa;
+        this.activity = activity;
+    }
+
+    public interface OnCustomItemClickListener {
+        void onEditClick(int position);
+        void onDeleteClick(int position);
+    }
+
+    public void setOnItemClickListener(OnCustomItemClickListener listener) { mListener = listener; }
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup viewGroup, int viewType) {
+        LayoutInflater inflater = activity.getLayoutInflater();
+        View view = inflater.inflate(R.layout.row_web_address, viewGroup, false);
+        ViewHolder viewHolder = new ViewHolder(view, mListener);
+        return viewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(WebAddressAdapter.ViewHolder viewHolder, int position) {
+        viewHolder.tvName.setText(wa.get(position).getName());
+        viewHolder.tvAddress.setText(wa.get(position).getAddress());
+        viewHolder.cardView.setOnClickListener(onClickListener(position));
+    }
+
+    private View.OnClickListener onClickListener(final int position) {
+        return v -> {
+            String url  = wa.get(position).getAddress();
+            openToBrowser(v, url);
+        };
+    }
+
+    public void openToBrowser(View v, String url){
+        if (!url.startsWith("http://") && !url.startsWith("https://"))
+            url = "http://" + url;
+        Uri webpage = Uri.parse(url);
+        Intent intent = new Intent(Intent.ACTION_VIEW, webpage);
+        v.getContext().startActivity(intent);
+        try {
+            v.getContext().startActivity(intent);
+        }
+        catch (Exception e){
+            if (url.startsWith("http://")) {
+                openToBrowser(v, url.replace("http://","https://"));
+            }
+        }
+    }
+
+    @Override
+    public int getItemCount() {
+        return (null != wa ? wa.size() : 0);
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        private TextView tvName, tvAddress;
+        private ImageView ivOptionMenu;
+        private View cardView;
+
+        public ViewHolder(View view, final OnCustomItemClickListener listener) {
+            super(view);
+            tvName = view.findViewById(R.id.tvName);
+            tvAddress = view.findViewById(R.id.tvAddress);
+            ivOptionMenu = view.findViewById(R.id.ivOptionMenu);
+            cardView = view.findViewById(R.id.cardView);
+
+            ivOptionMenu.setOnClickListener(v -> {
+                if(listener != null) {
+                    final int position = getAdapterPosition();
+                    Log.e("data adapter", wa.get(position).getName());
+                    if(position != RecyclerView.NO_POSITION) {
+                        final Context context = ivOptionMenu.getContext();
+                        PopupMenu popup = new PopupMenu(context, ivOptionMenu);
+                        popup.inflate(R.menu.option_menu_list);
+                        popup.setOnMenuItemClickListener(item -> {
+                            int itemId = item.getItemId();
+                            if (itemId == R.id.action_edit) {
+                                listener.onEditClick(position);
+                            } else {
+                                listener.onDeleteClick(position);
+                            }
+                            return true;
+                        });
+                        popup.show();
+                    }
+                }
+            });
+        }
+    }
+}
